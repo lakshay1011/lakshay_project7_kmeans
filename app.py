c06ae5f7173d9ffabb6b86956761ab679bdec754
lakshay1011 <83716870+lakshay1011@users.noreply.github.com>
2022-08-08 22:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 import pickle
 
 
 app = Flask(__name__)
 model = pickle.load(open('kmeans_project7.pkl','rb'))   
-run_with_ngrok(app)
+#run_with_ngrok(app)
 
 @app.route('/')
 def home():
@@ -41,4 +41,5 @@ def predict():
   return render_template('index.html', prediction_text='Model  has predicted  : {}'.format(result))
 
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
